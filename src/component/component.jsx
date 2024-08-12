de6fafd5a7235269f49e4beda39ac89234494138
sindhuja2212 <teppasindhuja@gmail.com>
2024-08-12 16:25:05 +0530

    components react

---
+++ src/component/component.jsx
@@ -0,0 +1,53 @@
+export const List =()=>{
+    return(
+        <ol>
+            <li>hi</li>
+            <li>hello</li>
+            <li>bye</li>
+        </ol>
+      
+    )
+
+}
+
+export const Table = ()=>{
+
+    return(
+        <table>
+            <tr>
+                <td>hi</td>
+                <td>hello</td>
+                <td>bye</td>
+                <td>good morning</td>
+            </tr>
+            <tr>
+                <th>names</th>
+                <td>sindhuja</td>
+                <td>soniya</td>
+                <td>manasa</td>
+            </tr>
+        </table>
+    )
+}
+
+export const Unorderlist = ()=>{
+    return(
+        <ul>
+            <li>sindhu</li>
+            <li>vidhya</li>
+            <li>manasa</li>
+            <li>mounika</li>
+            
+        </ul>
+    )
+}
+export const Cards = ()=>{
+    return(
+        <div class="container">
+            <img class="image"src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXJA32WU4rBpx7maglqeEtt3ot1tPIRWptxA&s">
+            </img>
+            <h2 class="title">hello</h2>
+            <p class="p">how r u</p>
+        </div>
+    )
+}
